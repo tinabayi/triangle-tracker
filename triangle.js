41ab59799ff34d3fabfcc079f35c9004c4064bd0
tinabayi <bayizerechristine4@gmail.com>
2018-12-15 10:38:05 +0200

    create a function in Javascript so that it can interract with html

--- triangle.js
+++ triangle.js
@@ -1,5 +1,33 @@
 
     function myFunction() {
-        var firstString = document.getElementById("myNumber").value;
-        alert(firstString);
-    }
+        var side1 = parseInt(document.getElementById("myNumber1").value);
+        var side2 =parseInt(document.getElementById("myNumber2").value);
+        var side3 = parseInt(document.getElementById("myNumber3").value);
+
+        var sides = []
+        var sides= sides.concat([side1, side2, side3]);
+
+        if((side1 + side2 <= side3 && side2+ side1 <= side3) || 
+            (side2 + side3 <= side1 && side3 + side2 <= side1) || 
+            (side1 + side3 <= side2 && side3 + side1 <= side2)){
+            alert("Is not a triangle")
+            return
+        }
+        
+       if(side1 === side2 && side1 === side3 && side2 === side3){
+            alert("Triangle is Equilateral")
+        }
+        else if(side1 === side2 || side1===side3 ||side2===side3){
+            alert("Triangle is isocele")
+        }
+        else if((side1+side2 > side3) || side1+side3 > side2 || side2+side3 > side1){
+            alert("Triangle is Scalene")
+        }
+        else{
+            alert('nothing')
+        }
+    };
+
+    
+    
+    
